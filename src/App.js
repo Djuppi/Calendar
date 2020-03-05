db75197e1a32a88cee4c6fa83036159c3290a87a
Aske <askeda@gmail.com>
2020-03-05 08:25:35 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {HashRouter, Route, Link} from 'react-router-dom'
+import './style.scss';
+
+import Calender from './components/Calender'
+import Week from './components/Week'
+import AddEvent from './components/AddEvent';
+import Signup from './components/Signup';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HashRouter >
+        <Route path="/" exact component={Calender} />
+        <Route path="/login" component={Login} />
+        <Route path="/week" component={Week} />
+        <Route path="/addevent" component={AddEvent} />
+        <Route path="/signup" component={Signup} />
+      </HashRouter>
     </div>
   );
 }
